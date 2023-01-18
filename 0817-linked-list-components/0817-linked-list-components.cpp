28d6337eaf617080cde4c86b6e3db7695dd3d6c9
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2023-01-18 16:47:17 +0530

    Time: 43 ms (78.84%), Space: 21.6 MB (39.20%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto e:nums)mp[e]++;
+        int c=0;bool flag=true;
+        ListNode*temp=head;
+//         while(temp->next)
+//             {mp[temp->next->val]=temp->val;
+//              temp=temp->next;
+//         n++;}
+//        vector<int>v(n+1,0);
+// sort(nums.begin(),nums.end());
+//         mp[head->val]=head->val;
+//         for(auto e:nums){
+//             if(!v[e] && !v[mp[e]]){
+//                 v[mp[e]]=1;
+//                 c++;
+//             }
+//             v[e]=1;
+//         }
+//         return c;
+        while(head){
+          if(mp.find(head->val)!=mp.end() && flag){
+               c++;
+               flag=false;
+           } 
+        else if(mp.find(head->val)==mp.end()) flag=true;
+        head=head->next;
+        }
+        return c;
+    }
+};
