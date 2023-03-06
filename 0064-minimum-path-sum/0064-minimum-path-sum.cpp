94d1dc1f6e424327ced87b2828352788870ccd40
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2023-03-06 17:15:02 +0530

    Time: 8 ms (81.62%), Space: 10.2 MB (35.13%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<int>>&dp){
-        if(i<0 || i>=m || j<0 || j>=n)return 1e6;
+        if(i<0 || i>=m || j<0 || j>=n)return 1e3;
         if(i==m-1 && j==n-1)return grid[i][j];
         if(dp[i][j]!=-1)return dp[i][j];
         return dp[i][j]= min(grid[i][j]+solve(grid,i+1,j,m,n,dp),grid[i][j]+solve(grid,i,j+1,m,n,dp));
