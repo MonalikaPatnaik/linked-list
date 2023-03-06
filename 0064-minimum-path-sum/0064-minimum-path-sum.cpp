016be3293e0a5f46b8ed6b9bfcab9da48a24f06a
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2023-03-06 17:39:05 +0530

    Time: 11 ms (60.19%), Space: 9.6 MB (91.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -9,19 +9,19 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n));
+        // vector<vector<int>>dp(m,vector<int>(n));
         // return solve(grid,0,0,m,n,dp);
         // dp[0][0]=grid[0][0];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 && j==0)dp[i][j]=grid[i][j];
-                else if(i==0){
-                    dp[i][j]=grid[i][j]+dp[i][j-1];
+                if(i==0 && j==0)grid[i][j]=grid[i][j];
+                else if(i==0 && j){
+                    grid[i][j]=grid[i][j]+grid[i][j-1];
                 }
-                else if(j==0)dp[i][j]=grid[i][j]+dp[i-1][j];
-                else dp[i][j]=min(grid[i][j]+dp[i-1][j],grid[i][j]+dp[i][j-1]);
+                else if(j==0 && i)grid[i][j]=grid[i][j]+grid[i-1][j];
+                else grid[i][j]=min(grid[i][j]+grid[i-1][j],grid[i][j]+grid[i][j-1]);
             }
         }
-        return dp[m-1][n-1];
+        return grid[m-1][n-1];
     }
 };
