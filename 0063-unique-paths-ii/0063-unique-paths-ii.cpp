5e97865a92e59b4379492b05ac3b64bb2c874d77
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2023-03-06 16:07:06 +0530

    Time: 3 ms (77.91%), Space: 7.8 MB (50.70%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -9,24 +9,39 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(obstacleGrid,0,0,obstacleGrid.size(),obstacleGrid[0].size(),dp);
+        vector<vector<int>>dp(m,vector<int>(n));
+        // return solve(obstacleGrid,0,0,obstacleGrid.size(),obstacleGrid[0].size(),dp);
         
         // int m=obstacleGrid.size();
         // int n=obstacleGrid[0].size();
         // if(obstacleGrid[m-1][n-1]==1)return 0;
-        
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(obstacleGrid[i][j]==1 &&(i==0 || j==0)){dp[m-1][n-1]=0;}
-        //          else if(obstacleGrid[i][j]!=1)dp[i][j]=1;
-        //         else{
-        //             if(dp[i-1][j]==0 && dp[i][j-1]==0)dp[i][j]=0;
-        //             else dp[i][j]=dp[i-1][j]+dp[i][j-1];
-        //         }
-        //     }
-        // }
-        // return dp[m-1][n-1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                    else dp[i][j]=1;
+                }
+                else if(i==0){
+                    if(dp[i][j-1]==0 || obstacleGrid[i][j]==1)dp[i][j]=0;
+                    else dp[i][j]=1;
+                }
+                else if(j==0){
+                    if(dp[i-1][j]==0 || obstacleGrid[i][j]==1)dp[i][j]=0;
+                    else dp[i][j]=1;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                // if(obstacleGrid[i][j]==1 &&(i==0 || j==0)){dp[m-1][n-1]=0;}
+                 if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                else{
+                    // if(dp[i-1][j]==0 && dp[i][j-1]==0)dp[i][j]=0;
+                     dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
